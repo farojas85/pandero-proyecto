512fb1f8a26f80d9d76aca6f79322fd008f52096
Fredy Rojas <farojas851@gmail.com>
2022-11-09 23:06:36 -0500

    Inicializando Proyecto

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,52 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+// console.log(import.meta.env.VITE_APP_ENV)
+
+
+export default ({mode}) => {
+
+    process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+
+    const envo = process.env.APP_ENV =='local' ? false : true
+
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: 'resources/js/app.js',
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
-            },
-        }),
-    ],
-});
+            }),
+        ],
+        server:{
+            https: envo
+        }
+    });
+}
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: 'resources/js/app.js',
+//             refresh: true,
+//         }),
+//         vue({
+//             template: {
+//                 transformAssetUrls: {
+//                     base: null,
+//                     includeAbsolute: false,
+//                 },
+//             },
+//         }),
+//     ],
+//     server:{
+//         //https: env
+//     }
+// });
